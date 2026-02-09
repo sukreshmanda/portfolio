ba10d5e3c5bcbca785b3ac922e47e131543beca4
sukreshmanda <mandasukresh@gmail.com>
2026-02-09 15:32:20 +0530

    Done

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +19,5 @@ export default defineConfig({
     },
   },
 
-  output: "server",
+  adapter: node({ mode: "standalone" }),
 });
